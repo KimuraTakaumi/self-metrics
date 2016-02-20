b89506d21d59deded29d7adc132aea1aeafa0e04
木村尭海 <muchiki0226@kimuratakashikai-no-MacBook-Pro.local>
2016-02-20 18:11:45 +0900

    Modify auto day request

--- js/metrics.js
+++ js/metrics.js
@@ -1,41 +1,68 @@
 angular.module('MetricsApp', [])
     .controller('MetricsController', ['$scope', '$http',  function($scope, $http) {
-        $scope.report = [
-            {number:1, work:'settings', time:'2H'},
-            {number:2, work:'settings', time:'2H'},
-            {number:3, work:'settings', time:'2H'},
-            {number:4, work:'settings', time:'2H'}];
-
+        $scope.report = [];
+        $scope.comment = '';
         var $uri ='config.json';
 
         $scope.init = function() {
-            $http({
-                method : 'GET',
-                url : 'http://localhost:3000/metrics/tkimura/2016/2/14'
-            }).success(function(data, status, headers, config) {
-                console.log(status);
-                console.log(data);
-                $scope.report = [];
-                var work = [];
-                for(i = 0; i< data.length; i++){
-                  var json = {};
-                  json['number'] = i;
-                  json['work'] = data[i].work;
-                  json['time'] = '';
-                  work[i] = new Date(data[i].date);
-                  console.log(work[i].getTime());
-                  $scope.report.push(json);
-                }
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'data/config.json', true);
+            xhr.onreadystatechange = function(){
+                if (xhr.readyState === 4 && xhr.status === 200){
+                    console.log(xhr.responseText);
+                    var json = JSON.parse(xhr.responseText);
 
-                for(i = 0; i< data.length - 1; i++){
-                  var time = work[i + 1] - work[i];
-                  var minutes = Math.round((time/(1000*60))%60)
-                  var hours = Math.round((time/(1000*60*60))/24);
-                  $scope.report[i]['time'] = hours + "H " + minutes + "M";
-                }
+                    var request_day = new Date();
+                    var request_url = json.url + '/' + json.user + '/' +request_day.getFullYear()+'/' +(request_day.getMonth()+1)+'/' + request_day.getDate();
+                    console.log(request_url);
+                    $http({
+                        method : 'GET',
+                        url :request_url
+                    }).success(function(data, status, headers, config) {
+                        console.log(status);
+                        console.log(data);
+                        $scope.report = [];
+                        var work = [];
+                        for(var i = 0; i< data.length; i++){
+                            var json = {};
+                            json['number'] = i;
+                            json['work'] = data[i].work;
+                            json['time'] = '';
+                            work[i] = new Date(data[i].date);
+                            console.log(work[i].getTime());
+                            $scope.report.push(json);
+                        }
+                        var json = {};
+                        json['number'] = data.length;
+                        json['work'] = '日報';
+                        json['time'] = '';
+                        work[i] = new Date();
+                        console.log(work[i].getTime());
+                        $scope.report.push(json);
 
-            }).error(function(data, status, headers, config) {
-                console.log(status);
-            });
+                        for(i = 0; i< data.length; i++){
+                            var time = work[i + 1] - work[i];
+                            var minutes = Math.round((time/(1000*60))%60)
+                            var hours = Math.round((time/(1000*60*60))/24);
+                            $scope.report[i]['time'] = hours + "H " + minutes + "M";
+                        }
+
+                    }).error(function(data, status, headers, config) {
+                        console.log(status);
+                    });
+
+
+                }
+            };
+            xhr.send(null);
+
+
+
+
+        };
+
+        $scope.onclick = function() {
+            console.log('onclick');
+            console.log($scope.comment);
         };
     }]);
--- main.js
+++ main.js
@@ -1,135 +1,112 @@
 'use strict';
 
 const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 var config;
 var items;
 var request = require('request');
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform != 'darwin') {
         app.quit();
     }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function () {
+var showWindow = function (url) {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL(url);
+    mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+};
+
+var init = function (next) {
     var fs = require('fs');
     fs.readFile('./data/config.json', 'utf8', function (err, text) {
         config = JSON.parse(text);
-
         fs.readFile('./data/items.json', 'utf8', function (err, text) {
             items = JSON.parse(text);
-            var Tray = require('tray');
-            var Menu = require('menu');
-
-            var appIcon = new Tray(__dirname + '/images/icon.png');
-
-            var array = [];
-            for (var i = 0; i < items.items.length; i++) {
-                var item = {};
-                var label = items.items[i];
-                item["label"] = items.items[i];
-                item["click"] = function () {
-                    console.log("test");
-
-                    var json = {};
-                    json["user"] = config.user;
-                    json["work"] = label;
-
-                    var options = {
-                        url: config.url,
-                        headers: {'Content-Type': 'application/json'},
-                        json: true,
-                        body: json
-                    };
-
-                    request.post(options, function (error, response, body) {
-                        if (!error && response.statusCode == 200) {
-                            console.log("success");
-                        } else {
-                            console.log('error: ' + response.statusCode);
-                        }
-                    });
-                };
-                array.push(item);
-            }
-
-            var item = {};
-            item["type"] = "separator";
-            array.push(item);
-
-            item = {};
-            item["label"] = "日報";
-            item["click"] = function () {
-                // Create the browser window.
-                mainWindow = new BrowserWindow({width: 800, height: 600});
-
-                // and load the index.html of the app.
-                mainWindow.loadURL('file://' + __dirname + '/metrics.html');
-
-                // Open the DevTools.
-                //mainWindow.webContents.openDevTools();
-
-                // Emitted when the window is closed.
-                mainWindow.on('closed', function () {
-                    // Dereference the window object, usually you would store windows
-                    // in an array if your app supports multi windows, this is the time
-                    // when you should delete the corresponding element.
-                    mainWindow = null;
-                });
-            };
-            array.push(item);
-            item = {};
-            item["type"] = "separator";
-            array.push(item);
-
-            item = {};
-            item["label"] = "設定";
-            item["click"] = function () {
-                // Create the browser window.
-                mainWindow = new BrowserWindow({width: 800, height: 600});
-
-                // and load the index.html of the app.
-                mainWindow.loadURL('file://' + __dirname + '/config.html');
-
-                // Open the DevTools.
-                //mainWindow.webContents.openDevTools();
-
-                // Emitted when the window is closed.
-                mainWindow.on('closed', function () {
-                    // Dereference the window object, usually you would store windows
-                    // in an array if your app supports multi windows, this is the time
-                    // when you should delete the corresponding element.
-                    mainWindow = null;
-                });
-            };
-            array.push(item);
-            item = {};
-            item["type"] = "separator";
-            array.push(item);
-
-            item = {};
-            item["label"] = "終了";
-            item["click"] = function () {
-                app.quit();
-            };
-            array.push(item);
-
-            var contextMenu = Menu.buildFromTemplate(array);
-            appIcon.setContextMenu(contextMenu);
-            appIcon.setToolTip('Self Metrics');
+            next();
         });
+    });
+};
 
+var createItem = function (label) {
+    var item = {};
+    item["label"] = label;
+    item["click"] = function () {
+        var json = {};
+        json["user"] = config.user;
+        json["work"] = label;
+
+        var options = {
+            url: config.url,
+            headers: {'Content-Type': 'application/json'},
+            json: true,
+            body: json
+        };
+
+        request.post(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("success");
+            } else {
+                console.log('error: ' + response.statusCode);
+            }
+        });
+    };
+    return item;
+};
+
+app.on('ready', function () {
+    init(function () {
+        var Tray = require('tray');
+        var Menu = require('menu');
+
+        var appIcon = new Tray(__dirname + '/images/icon.png');
+
+        var array = [];
+        for (var i = 0; i < items.items.length; i++) {
+            var item = createItem(items.items[i]);
+            array.push(item);
+        }
+
+        var item = {};
+        item["type"] = "separator";
+        array.push(item);
+
+        item = {};
+        item["label"] = "日報";
+        item["click"] = function () {
+            showWindow('file://' + __dirname + '/metrics.html');
+        };
+        array.push(item);
+        item = {};
+        item["type"] = "separator";
+        array.push(item);
+
+        item = {};
+        item["label"] = "設定";
+        item["click"] = function () {
+            showWindow('file://' + __dirname + '/config.html');
+        };
+        array.push(item);
+        item = {};
+        item["type"] = "separator";
+        array.push(item);
+
+        item = {};
+        item["label"] = "終了";
+        item["click"] = function () {
+            app.quit();
+        };
+        array.push(item);
+
+        var contextMenu = Menu.buildFromTemplate(array);
+        appIcon.setContextMenu(contextMenu);
+        appIcon.setToolTip('Self Metrics');
     });
 });
 
